b929114d06d9fbd6e9a43181d4947e0bf6447c79
AlexGood980 <sasha2286980@gmail.com>
2016-02-25 00:17:09 +0200

    cards_done

--- app/routes/card.js
+++ app/routes/card.js
@@ -3,16 +3,6 @@ var User    = require("../../app/models/user"); // load up the user model
 /**
  * save card request
  */
-<<<<<<< HEAD
-exports.addcard = function(req, res) {
-  var listId    = req.params.id;
-  var cardText  = req.body.text;
-  var queryUser = { _id: req.user._id };
-
-  User
-    .findOne(queryUser)
-    .select("lists._id lists.cards")
-=======
 exports.addCard = function(req, res) {
 
   var queryUser = { name: req.params.user };
@@ -21,7 +11,6 @@ exports.addCard = function(req, res) {
 
   User
     .findOne(queryUser)
->>>>>>> 1d27a25dbc414c5b7dc3e0b3955226d63041dfc5
     .exec(function (err, user) {
       if (err) throw err;
 
@@ -31,11 +20,8 @@ exports.addCard = function(req, res) {
         text: cardText,
       });
 
-<<<<<<< HEAD
-=======
       console.log(list.cards)
 
->>>>>>> 1d27a25dbc414c5b7dc3e0b3955226d63041dfc5
       user.save(function(err, done) {
         if (err) return done(err);
 
@@ -44,102 +30,6 @@ exports.addCard = function(req, res) {
     });
 };
 
-<<<<<<< HEAD
-/**
- * get one card request
- */
-exports.getcard = function(req, res) {
-  var listId    = req.params.id_list;
-  var cardId    = req.params.id_card;
-  var queryUser = { _id: req.user._id };
-
-  User
-    .findOne(queryUser)
-    .select("lists._id lists.cards")
-    .exec(function (err, user) {
-      if (err) throw err;
-
-      var card = user.lists.id(listId).cards.id(cardId);
-      res.json({ card: card });
-  });
-};
-
-/**
- * get all cards request
- */
-exports.getcards = function(req, res) {
-  var listId    = req.params.id_list;
-  var queryUser = { _id: req.user._id };
-
-  User
-    .findOne(queryUser)
-    .select("lists._id lists.cards")
-    .exec(function (err, user) {
-      if (err) throw err;
-
-      var cards = user.lists.id(listId).cards;
-      res.json({ cards: cards });
-  });
-};
-
-/**
- * remove one card request
- */
-exports.removecard = function(req, res) {
-  var listId    = req.params.id_list;
-  var cardId    = req.params.id_card;
-  var queryUser = { _id: req.user._id };
-
-  User
-    .findOne(queryUser)
-    .select("lists._id lists.cards")
-    .exec(function (err, user) {
-      if (err) throw err;
-
-      user.lists.id(listId).cards.id(cardId).remove();
-      user.save(function (err, done) {
-        if (err) return done(err);
-
-        res.json({ success: true });
-      });
-  });
-};
-
-/**
- * put changes in card
- */
-exports.updatecard = function(req, res, next) {
-  var listId    = req.params.id_list;
-  var cardId    = req.params.id_card;
-  var queryUser = { _id: req.user._id };
-
-  User
-    .findOne(queryUser)
-    .select("lists._id lists.cards")
-    .exec(function (err, user) {
-      if (err) throw err;
-
-      var card = user.lists.id(listId).cards.id(cardId);
-
-      if (req.body.completed) {
-        card.completed = (card.completed)
-          ? false
-          : true;
-      } else if (req.body.color) {
-        card.color = req.body.color;
-      } else if (req.body.text) {
-        card.text = req.body.text;
-      }
-
-
-      user.save(function (err, done) {
-        if (err) return done(err);
-
-        res.json({ card: card });
-      });
-  });
-};
-=======
 // /**
 //  * get one card request
 //  */
@@ -234,4 +124,3 @@ exports.updatecard = function(req, res, next) {
 //       });
 //   });
 // };
->>>>>>> 1d27a25dbc414c5b7dc3e0b3955226d63041dfc5
